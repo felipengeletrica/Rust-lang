52d4469f59a84e21bdda771772ec67febf6696eb
Felipe Vargas <felipeng.eletrica@gmail.com>
2020-12-23 10:52:24 -0300

    Add vectors.

---
+++ commom_collections/vector/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+
+    println!("Vector");
+    let _v: Vec<i32> = Vec::new();
+}
--- package_crates_modules/paths/src/main.rs
+++ package_crates_modules/paths/src/main.rs
@@ -1,8 +1,12 @@
 use std::collections::HashMap;
+use rand::Rng;
 
 fn main() {
     let mut map = HashMap::new();
+    let secret_number = rand::thread_rng().gen_range(1, 101);
     map.insert(1, 2);
+    map.insert(2, 2);
 
     println!("Map:{:?}", map);
+    println!("Secrete number: {}", secret_number);
 }
